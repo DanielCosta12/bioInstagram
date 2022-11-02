add6c5e0f027e59d153441d015b2525ea4942064
Daniel <danieldossantosdacosta@gmail.com>
2022-11-02 12:08:55 -0300

    import button and add in template

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 import Top from "./components/Top.vue";
 import SectionMain from "./components/SectionMain.vue";
+import Button from "./components/Button.vue";
 
 components: {
-  Top, SectionMain;
+  Top, SectionMain, Button;
 }
 </script>
 
 <template>
+  <Button />
   <Top />
   <SectionMain />
 </template>
