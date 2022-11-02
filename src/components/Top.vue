c2f9b9e831101cde473a3dd9c04336324e379ea6
Daniel <danieldossantosdacosta@gmail.com>
2022-11-02 13:18:27 -0300

    fix main img

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <div>
-      <img class="pfp" :src="imgSrc" alt="Foto de Perfil" />
+      <img class="pfp" src="../assets/pfpdc.png" alt="Foto de Perfil" />
       <br />
       <h1>{{ h1 }}</h1>
       <p>"{{ p }}" {{ donda }}</p>
@@ -13,7 +13,6 @@ export default {
   name: "Top",
   data() {
     return {
-      imgSrc: "../public/pfpdc.png",
       h1: "ne_danz",
       p: "The giant looks in the mirror and sees nothing",
       donda: " -Donda West",
